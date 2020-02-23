06948d3dfc48750c64de2898734dd57e13b4e892
Fer Torres <fertorresmx@gmail.com>
2020-02-22 21:53:04 -0600

    Formating document

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,32 +1,36 @@
 import React from "react";
 
-import './styles/Badge.css';
-import confLogo from '../images/badge-header.svg';
+import "./styles/Badge.css";
+import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
-    render() {
-        return (
-            <div className="Badge">
-                <div className="Badge__header">
-                    <img src={confLogo} alt="Logo de la conferencia"/>
-                </div>
+  render() {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
 
-                <div className="Badge__section-name">
-                    <img className="Badge__avatar" src="https://s.gravatar.com/avatar/df4dcd1833595186c053f0cf369a6c79?s=80" alt="Avatar"/>
-                    <h1>Fernando <br/> Torres</h1>
-                </div>
+        <div className="Badge__section-name">
+			<img
+            className="Badge__avatar"
+            src="https://s.gravatar.com/avatar/df4dcd1833595186c053f0cf369a6c79?s=80"
+            alt="Avatar"
+          />
+          <h1>
+            Fernando <br /> Torres
+          </h1>
+        </div>
 
-                <div className="Badge__section-info">
-                    <h3>Data Scientist</h3>
-                    <div>@fertorresmx</div>
-                </div>
+        <div className="Badge__section-info">
+          <h3>Data Scientist</h3>
+          <div>@fertorresmx</div>
+        </div>
 
-                <div className="Badge__footer">
-                    #platziconf
-                </div>
-            </div>
-        )
-    }
+        <div className="Badge__footer">#platziconf</div>
+      </div>
+    );
+  }
 }
 
 export default Badge;
