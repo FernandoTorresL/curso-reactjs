606836d7b3568d23f9dd852037aa398948d7a469
Fer Torres <fertorresmx@gmail.com>
2020-02-22 23:54:49 -0600

    Adding profesor's code

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,35 +1,64 @@
 import React from 'react';
 
-import '../components/styles/Badge.css';
+import './styles/BadgeNew.css';
 import header from '../images/badge-header.svg';
-import Navbar from '../components/Navbar';
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
+import Navbar from '../components/Navbar';
 
 class BadgeNew extends React.Component {
-	render() {
-		return (
-			<div>
-				<Navbar />
-				<div className="BadgeNew__hero">
-					<img className="img-fluid" src={header} alt="Logo"/>
-				</div>
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: '',
+    },
+  };
 
-				<div className="container">
-					<div className="row">
-						<div className="col">
-							<Badge
-								firstName="Fernando"
-								lastname="Torres"
-								twitter="fertorresmx"
-								jobTitle="Data Scientist"
-								avatar="https://s.gravatar.com/avatar/df4dcd1833595186c053f0cf369a6c79?s=80"
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
+                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+              />
+            </div>
+
+            <div className="col-6">
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BadgeNew;
+
